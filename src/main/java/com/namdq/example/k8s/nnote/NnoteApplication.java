dc5c373a1f23fa27e729befa44c1a54a130a8cb8
Nam Dang Quoc <namquoc.dev@gmail.com>
2020-03-26 17:57:52 +0700

    init project

---
+++ src/main/java/com/namdq/example/k8s/nnote/NnoteApplication.java
@@ -0,0 +1,13 @@
+package com.namdq.example.k8s.nnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NnoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/namdq/example/k8s/nnote/NnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namdq.example.k8s.nnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
